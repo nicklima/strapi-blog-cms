03d6b19448eb24a9e42cd18da1b4269ea6932e79
Nick Lima <nick.lima.wp@gmail.com>
2022-05-20 09:02:08 -0300

    💚 fixing build error

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'ea1650efe80d6adad8677cdd2a618db1'),
+    secret: env("ADMIN_JWT_SECRET"),
+  },
+  apiToken: {
+    salt: env("API_TOKEN_SALT"),
   },
 });
